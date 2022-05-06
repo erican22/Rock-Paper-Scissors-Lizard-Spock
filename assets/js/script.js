3bc4c65f1f0736461c3f5a3ef8e376693c6bca4d
erican22 <erica.naslund@live.se>
2022-05-06 15:09:45 +0000

    Add functions to increase score on page, and call score functions in gameCompare function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,25 +4,47 @@ let computerChoice = "Paper";
 function gameCompare(playerOne, playerTwo) {
     if (playerOne === playerTwo) {
         console.log("It is a draw")
+        drawIncrease ();
     }
     else if (playerOne === "Rock" && (playerTwo === "Lizard" || playerTwo === "Scissors")) {
         console.log("Player One wins");
+        userOneWin ();
     }
     else if (playerOne === "Paper" && (playerTwo === "Rock" || playerTwo === "Spock")) {
         console.log("Player One wins");
+        userOneWin ();
     }
     else if (playerOne === "Scissors" && (playerTwo === "Paper" || playerTwo === "Lizard")) {
         console.log("Player One wins");
+        userOneWin ();
     }
     else if (playerOne === "Lizard" && (playerTwo === "Spock" || playerTwo === "Paper")) {
         console.log("Player One wins");
+        userOneWin ();
     }
     else if (playerOne === "Spock" && (playerTwo === "Rock" || playerTwo === "Scissors")) {
         console.log("Player One wins");
+        userOneWin ();
     }
     else {
         console.log("Player Two wins");
+        userTwoWin ();
     }
 }
 
 gameCompare(userChoice, computerChoice);
+
+function userOneWin () {
+    let oldScore = parseInt(document.getElementById('user-score').textContent);
+    document.getElementById('user-score').innerHTML = oldScore + 1;
+}
+
+function userTwoWin () {
+    let oldScore = parseInt(document.getElementById('computer-score').textContent);
+    document.getElementById('computer-score').innerHTML = oldScore + 1;
+}
+
+function drawIncrease () {
+    let oldScore = parseInt(document.getElementById('draws').textContent);
+    document.getElementById('draws').innerHTML = oldScore + 1;
+}
